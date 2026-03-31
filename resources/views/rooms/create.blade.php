5b6b44c3421cbb2d227dde274447642eaf4b0365
ebubelife <ebubeemeka19@gmail.com>
2026-03-31 09:03:01 +0100

    ...

--- resources/views/rooms/create.blade.php
+++ resources/views/rooms/create.blade.php
@@ -295,6 +295,7 @@ function roomCreation() {
             payment_type: 'full',
             party_b_email: ''
         },
+        
         categories: [
             { value: 'tenancy', label: 'Tenancy', description: 'Landlord-tenant disputes', color: '#1D4ED8', icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6' },
             { value: 'freelance', label: 'Freelance', description: 'Contract & payment issues', color: '#15803D', icon: 'M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z' },
